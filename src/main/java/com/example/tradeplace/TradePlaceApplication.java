69dbd299a0db9be0d159a95ab482aaea0be80828
Cothn <48108190+Cothn@users.noreply.github.com>
2023-02-11 10:49:53 +0300

    Create_SpringBoot_project

---
+++ src/main/java/com/example/tradeplace/TradePlaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.tradeplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradePlaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradePlaceApplication.class, args);
+    }
+
+}
