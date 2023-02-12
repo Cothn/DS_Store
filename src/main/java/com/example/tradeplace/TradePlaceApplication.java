98af960c6de4d21cbe7b20699ac95b41bf6bfd4a
Cothn <48108190+Cothn@users.noreply.github.com>
2023-02-12 17:46:36 +0300

    Add hibernate configuration
    add hibernate configuration
    and change some application properties

--- src/main/java/com/example/tradeplace/TradePlaceApplication.java
+++ src/main/java/com/example/tradeplace/TradePlaceApplication.java
@@ -2,12 +2,19 @@ package com.example.tradeplace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+
+/**
+ * @author Vsevolod Grinchick
+ */
+@SpringBootApplication(exclude = { //
+        DataSourceAutoConfiguration.class, //
+        HibernateJpaAutoConfiguration.class })
 public class TradePlaceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TradePlaceApplication.class, args);
     }
-
 }
